e4db0fcc75c10ad75bd79de85df48a153dc81a1d
matbalez <matbalez@gmail.com>
2015-10-19 02:22:52 -0400

    Done.

--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,35 @@
 # Implement your object-oriented solution here!
+class LargestPalindromeProduct
+
+  def answer
+    largest = 0
+    test = 0
+    for i in 999.downto(100) do
+      for j in 999.downto(100) do
+        test = i*j
+        if is_palindrome?(test)
+          if test > largest
+            largest = test
+          end
+        end
+      end
+    end
+    largest
+  end
+
+  def is_palindrome?(num)
+    arr = num.to_s.chars.to_a
+    max = (arr.length - 1) / 2
+    i = 0 
+    match = true
+    while i <= max && match do
+      if arr[i] != arr[arr.length - 1 - i]
+        match = false
+      end
+      i += 1
+    end
+    match
+  end
+
+end
+
